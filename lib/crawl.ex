588f72931bb47b5bfccffd302bc2a20ffe46ba23
Ge Hao <althoughghgh@gmail.com>
2019-07-16 17:58:45 +0900

    cc

--- lib/crawl.ex
+++ lib/crawl.ex
@@ -1,63 +1,50 @@
 require IEx
 
 defmodule Crawl do
-  def detail_page(url) do
-    IO.puts(url)
-    {:ok, response} = HTTPoison.get(url, [], [follow_redirect: true])
-
-    [[_, views, upvote, downvote]] = Regex.scan(~r/nb-views-number">(.+?)<\/str.+?rating.+?>(.+?)<\/span>.+?rating.+?>(.+?)<\/span>/, response.body)
-    tags = Regex.scan(~r/\/tags\/(.+?)"/, response.body) |>
-            Enum.map(fn ele -> Enum.at(ele, 1) end)
-    time = case Regex.scan(~r/"duration">(.+?)<\/span>/, response.body) do
-      [[_, time]] -> time
-      _ -> ""
-    end
-    resolution = case Regex.scan(~r/video-hd-mark">(.+?)</, response.body) do
-      [[_, res]] -> res
-      _ -> ""
-    end
-
-    %{ views: views, upvote: upvote, downvote: downvote, tags: tags, time: time, resolution: resolution }
-  end
-
-  def main(listen_pid) do
-    base_url = "https://www.xvideos.com"
+  def main do
     HTTPoison.start
-    {:ok, response} = HTTPoison.get(base_url)
-    matches = Regex.scan(~r/data-id="(.+?)".+?<p class="title"><a href="(.+?)".+?title="(.+?)"/, response.body)
-    Enum.each(Enum.with_index(matches), fn ele ->
-      {[_, id, href, title], idx} = ele
-      # if idx < 5 do
-        # Task.async(fn ->
-          msg = detail_page(base_url<>href) |>
-                    Map.merge(%{id: id, href: href, title: title})
-          send(listen_pid, msg)
-        # end) |> Task.await
-      # end
-    end)
+    first_url = "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ra=013&ae=06401&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&rn=0640"
+    {:ok, pid} = Crawl.Data.run
+    process(pid, first_url)
   end
 
-  def save(msg) do
-    tags = "{\"" <> Enum.join(msg[:tags], "\",\"") <> "\"}"
-    "INSERT INTO crawl (site_id,title,href,views,upvote,downvote,tags,time,resolution) VALUES ('#{msg[:id]}','#{msg[:title]}','#{msg[:href]}','#{msg[:views]}','#{msg[:upvote]}','#{msg[:downvote]}','#{tags}','#{msg[:time]}','#{msg[:resolution]}')"
-  end
+  def process(pid, url) do
+    IO.puts(url)
+    {:ok, response} = HTTPoison.get(url)
 
-  def listen(sql_pid) do
-    receive do
-      msg ->
-        IO.puts(inspect(msg))
-        Postgrex.query!(sql_pid, save(msg), [])
+    unless exist(pid, url) do
+      save(pid, url, response.body)
+
+      links = response.body
+        |> Floki.find("body a")
+        |> Floki.attribute("href")
+        |> Enum.filter(&Regex.match?(~r/chintai\//, &1))
+        |> Enum.uniq()
+
+      Enum.each(links, fn link ->
+        fulllink = URI.merge("https://suumo.jp", link)
+                    |> URI.to_string
+                    |> String.trim
+        process(pid, fulllink)
+      end)
     end
-    listen(sql_pid)
   end
 
-  def establish do
-    {:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", password: "", database: "gehao")
-    pid
+  def save(pid, url, body) do
+    send(pid, {self(), :create, %{url: url, body: body}})
   end
+
+  def exist(pid, url) do
+    send(pid, {self(), :exist, url})
+    receive do
+      value -> 
+        IO.puts("#{value}, #{url}")
+        value
+    end
+  end
+
 end
 
-
-# sql_pid = Crawl.establish
-# listen = spawn(Crawl, :listen, [sql_pid])
-# Crawl.main(listen)
+# Crawl.establish
+# listen_pid = spawn(Crawl, :listen, [sql_pid])
+# Crawl.main(listen_pid)
---
+++ lib/crawl/data.ex
@@ -0,0 +1,37 @@
+defmodule Crawl.Data do
+  def run do
+    :application.ensure_all_started(:postgrex)
+    {:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", password: "", database: "gehao")
+    
+    Task.start_link(fn -> loop(pid) end)
+  end
+
+  def exist?(url) do
+    "SELECT id FROM suumo WHERE url='#{url}'"
+  end
+
+  def create(map) do
+    keys = Map.keys(map)
+    values = Enum.map(keys, fn key ->
+      "'#{String.replace(map[key], "'", "''")}'"
+    end) |> Enum.join(", ")
+
+    "INSERT INTO suumo (#{Enum.join(keys, ", ")}) VALUES (#{values})"
+  end
+
+  def loop(pid) do
+    receive do
+      {caller, :create, map} ->
+        Postgrex.query!(pid, create(map), [])
+      {caller, :exist, url} ->
+        %{num_rows: num_rows} = Postgrex.query!(pid, exist?(url), [])
+        result = cond do
+          num_rows > 0 -> true
+          true -> false
+        end
+
+        send(caller, result)
+    end
+    loop(pid)
+  end
+end
