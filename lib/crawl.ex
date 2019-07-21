adaf657680cdd212c886f9066e10dcbeb457456d
Ge Hao <althoughghgh@gmail.com>
2019-07-21 15:58:39 +0900

    url

--- lib/crawl.ex
+++ lib/crawl.ex
@@ -1,11 +1,11 @@
 require IEx
 
 defmodule Crawl do
-  def start_link do
+  def start_link(url) do
     HTTPoison.start
     Crawl.Data.start_link
 
-    first_url = "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ra=011&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&ek=064053820&ek=064053840&rn=0640"
+    first_url = url
     # if exist?(first_url) do
     #   IO.puts("cccccccccccc")
     #   %{rows: [[_, first_url]]} = Crawl.Data.last
