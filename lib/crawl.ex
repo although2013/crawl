050ffe7731e5cccf5e8bf19a869fb3c9e3a77084
Ge Hao <althoughghgh@gmail.com>
2019-07-29 00:00:45 +0900

    e

--- lib/crawl.ex
+++ lib/crawl.ex
@@ -7,7 +7,7 @@ defmodule Crawl do
     Crawl.Counter.start_link
     Crawl.Queue.start_link
 
-    first_url = "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ra=013&ae=01201&ae=01601&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&rn=0005&rn=0125&rn=0160&rn=0500&rn=0185&rn=0190&rn=0120&rn=0305&rn=0325&rn=0330&rn=0335&rn=0340&rn=0395&rn=0405&rn=0430&rn=0573&rn=0580&rn=0600&rn=0010&rn=0015&rn=0020&rn=0025&rn=0030&rn=0040&rn=0045&rn=0050&rn=0043&rn=0035&rn=0350&rn=0370&rn=0355&rn=0365&rn=0320&rn=0310&rn=0315&rn=0345&rn=0375&rn=0385&rn=0440&rn=0445&rn=0450&rn=0220&rn=0230&rn=0200&rn=0205&rn=0210&rn=0215&rn=0235&rn=0055&rn=0060&rn=0065&rn=0070&rn=0085&rn=0765&rn=0265&rn=0270&rn=0275&rn=0280&rn=0285&rn=0290&rn=0295&rn=0540&rn=0550&rn=0555&rn=0095&rn=0100&rn=0240&rn=0250&rn=0300&rn=0090&rn=0560&rn=0080&rn=0075&rn=0640&rn=0760&rn=7580&rn=7585&rn=0155&rn=0415&rn=0420&rn=0435&rn=0563"
+    first_url = "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=11&sc=11101&sc=11102&sc=11103&sc=11104&sc=11105&sc=11106&sc=11107&sc=11108&sc=11109&sc=11110&sc=11201&sc=11202&sc=11203&sc=11208&sc=11219&sc=11223&sc=11224&sc=11237&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2="
 
     Task.start_link(fn -> uniq_queue() end)
     Task.start_link(fn -> process_first(first_url) end)
